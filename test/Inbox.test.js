45d660c27c5cd0d1724601753ac98f216a567970
Mada Aflak <maflak@octo.com>
2018-06-02 15:24:34 -0700

    test contract deployement/updates

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,33 +1,43 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
-const { interface, bytecode} = require('../compile');
 
-const web3 = new Web3(ganache.provider());
+// UPDATE THESE TWO LINES RIGHT HERE!!!!! <-----------------
+const provider = ganache.provider();
+const web3 = new Web3(provider);
 
-// Flow :
-// mocha starts
-// deploy a new contract
-// manipulate the cntract
-// make an assertion about the contract
+const { interface, bytecode } = require('../compile');
 
 let accounts;
 let inbox;
+let inboxChanged;
 
-beforeEach(async ()=>{
-  // get a lost of all account
-  // web3.eth.getAccounts().then(fetchAccounts=>{
-  //   console.log(fetchAccounts);
-  // })
+beforeEach(async () => {
+  // Get a list of all accounts
   accounts = await web3.eth.getAccounts();
-  // use one of those accounts to deploy the contract
-  inbox = await new web3.eth.Contract(JSON.parse(interface)).deploy({ data: bytecode, arguments:['Hi there !']}).send({from: accounts[0], gas:'1000000'});
+  // Use one of those accounts to deploy the contract
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ['Hi there!'] })
+    .send({ from: accounts[0], gas: '1000000' });
 
+  inbox.setProvider(provider);
 });
 
-describe('deploy contract', () => {
-  it('okok ', ()=>{
-    console.log(accounts);
-    console.log(inbox);
-  })
+describe('Inbox', () => {
+
+  it('deploys a contract', () => {
+    assert.ok(inbox.options.address);
+  });
+  
+  it('has a default message', async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, 'Hi there!');
+  });
+
+  it('can change mmessage', async () => {
+    await inbox.methods.setMessage('By there !').send({ from: accounts[0], gas: '1000000' });
+    const message = await inbox.methods.message().call();
+    assert.equal(message,'By there !');
+  });
+
 });
